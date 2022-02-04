e62d9b705762f4507042b2781ea6bb18a44c3f32
nstave <nstave@cs.washington.edu>
2022-02-04 15:10:16 -0800

    login and signup working

--- website/app.py
+++ website/app.py
@@ -1,44 +1,122 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
-from forms import RegistrationForm, LoginForm
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from forms import LoginForm, SignupForm, EditProfileForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.urls import url_parse
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
+from config import Config
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'MINO_SONG'
+app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = 'login'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 people = ['Samuel Awuah', 'Shujian Lao', 'Will J Lee', 'Christin Lin', 'Mino Song', 'Noah S Staveley']
 
+# classes 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    DOB = db.Column(db.String(4), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+# ROUTES
+
+#@app.route("/")
+#def index():
+#    return render_template('index.html')
 
 @app.route("/")
+@app.route('/index')
+@login_required
 def index():
-    return render_template('index.html', people=people)
+    return render_template("index_login.html", title='Home Page')
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
 
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'sxp0728@126.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('index'))
-        else:
-            flash('Login unsuccessful. Please check the username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Log In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
-@app.route("/register", methods=["POST", "GET"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    return render_template('signup.html', title='Register', form=form)
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        #user.set_DOB(form.DOB.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Sign up', form=form)
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.DOB = form.DOB.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.DOB.data = current_user.DOB
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/forms.py
+++ website/forms.py
@@ -1,18 +1,38 @@
 from ast import Pass
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators = [DataRequired(), Email()])
-    password = PasswordField('Password', validators = [DataRequired()])
-    password_second = PasswordField('Comfire Password', validators = [DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
-    
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+import app
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators = [DataRequired(), Email()])
-    password = PasswordField('Password', validators = [DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+    
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    DOB = StringField('DOB (MM/DD/YY)', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+        user = app.User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = app.User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+        
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    DOB = StringField('DOB (MM/DD/YY)', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    

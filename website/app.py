edaab1bc5f742d276f08bb28445062472f83d638
nstave <nstave@cs.washington.edu>
2022-02-22 12:51:58 -0800

    implemented a packet queue

--- website/app.py
+++ website/app.py
@@ -13,7 +13,8 @@ import urllib
 from bs4 import BeautifulSoup
 import re # for regex
 from flask_mail import Mail
-
+import queue
+from queue import Empty
 
 import random
 import numpy as np
@@ -33,13 +34,20 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 
+'''
+    if packet_queue.empty():
+        packet_queue = fill_queue(user, packet_queue)                    # complete initial queue fill
+        flash('packet queue was filled')
+'''
+packet_queue = queue.Queue()
+
 # admin class to store admin users 
 class people():
     people = ['Samuel Awuah', 'Shujian Lao', 'Will J Lee', 'Christin Lin', 'Mino Song', 'Noah S Staveley']
     usernames = ['samuel_awuah, shujian_lao', 'will_lee', 'christin_lin', 'mino_song', 'noah_s', '_admin_']
     def __repr__(self):
         return 'People {}>'.format(self.people)
-    
+
 # User class for db
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -48,7 +56,7 @@ class User(UserMixin, db.Model):
     DOB = db.Column(db.String(6), index=True, unique=True)              # DOB field format is validated
     password_hash = db.Column(db.String(128))
     packets = db.relationship("Packet", backref='author', lazy='dynamic')
-    packet_count = db.Column(db.Integer, index=True, unique=False)
+    packet_count = db.Column(db.Integer, index=True)                    # TODO: managing the users packet counter
     admin = db.Column(db.Integer, index=True, default=False)
 
     def __repr__(self):
@@ -90,8 +98,8 @@ class User(UserMixin, db.Model):
 class Packet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(500))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)     # this timestamp represents when the test was ordered
-    test_taken = db.Column(db.String(50), index=True, default=None)               # this timestamp represents when the test was taken
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)         # this timestamp represents when the test was ordered
+    test_taken = db.Column(db.String(50), index=True, default=None)                 # this timestamp represents when the test was taken
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     test_type = db.Column(db.String(150), default=None)
 
@@ -99,7 +107,6 @@ class Packet(db.Model):
         self.id = id
         self.user_id = User.id
         return '<Packet {}>'.format(self.body)   
-                                        
     
 # APP ROUTES
 
@@ -155,6 +162,7 @@ def logout():
 # route for sign up page --> creates new user in database 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    global packet_queue
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = SignupForm()
@@ -164,6 +172,7 @@ def signup():
         user.validate_DOB(form.DOB.data)
         user.validate_email(form.email.data)
         user.set_admin(form.username.data)
+        user.packet_count = 0
         db.session.add(user)
         db.session.commit()
         MESSAGE = 'Congratulations ' + user.username + ' you are now a registered user!'
@@ -204,8 +213,16 @@ def edit_profile():
 # route for the grab new test data button on user profile page    
 @app.route('/newtest/<username>')
 def newtest(username):
+    global packet_queue
     user = User.query.filter_by(username=username).first_or_404()
-    new_packet(user) # call packet create helper function
+    
+    if (packet_queue.empty()): # if the queue is, first refill then call new packet
+        flash('queue was empty when new test was called') # for testing
+        packet_queue = fill_queue(user, packet_queue)  
+        new_packet(user, packet_queue) # call packet helper w global packet_queue
+    else:
+        flash('queue was NOT empty when new test was called') # for testing
+        new_packet(user, packet_queue) # call packet helper w global packet_queue
     # render user page again with new packet added
     return render_template('user.html', user=user, packets=user.packets.all())
 
@@ -221,26 +238,56 @@ def delete_all_tests(username):
 def deletetest(username, packet_id):
     user = User.query.filter_by(username=username).first_or_404()
     p = Packet.query.filter_by(id=packet_id).first_or_404()
+    #db.session.add(user)
     db.session.delete(p)
     db.session.commit()
     return render_template('user.html', user=user, packets=user.packets.all())  
 
 
-# this function grabs data from a thingspeak channel
-# creates a new packet and inserts the data into the body field
-def new_packet(user):
+# this function loops through all the rows in our thingspeak csv table
+# and creates a new packet for every row 
+# packets are stored in packet_queue and sent to new_packet(user, packet_queue)
+
+# newtest() will call fill_queue whenever packet_queue is empty
+# TODO: 
+# we only want to add packets with new test result data to the queue 
+# update this funciton so that it will disregard rows w that have been grabbed before
+#  I plan to do this by storing entry_ids and checking every row's entry id
+def fill_queue(user, packet_queue):
     thingspeak_read = urllib.request.urlopen('https://api.thingspeak.com/channels/1649676/feeds.csv?api_key=JLVZFZMPYNBHIU33')
     bytes_csv = thingspeak_read.read()
     data=str(bytes_csv,'utf-8')
     thingspeak_data = pd.read_csv(StringIO(data))
-    body_raw = pd.DataFrame(thingspeak_data)
-    body_contents = thingspeak_data.iloc[-1] # isolates most recent row (bottom row)
-    # create a new packet associated for user 
-    p = Packet(body=str(body_contents['field3']), author=user, test_type=str(body_contents['field2']), test_taken= "Date: "+ str(body_contents['field4'])+" Time: "+str(body_contents['field5'])) # field2 = test_type
-    # add new packet to db
-    db.session.add(p)
-    db.session.commit()
-    
+    body_df = pd.DataFrame(thingspeak_data)
+    # creates new packet for every row in index and adds packet to our packet_queue 
+    for i in range(1,len(body_df.index) +1): 
+        this_row = thingspeak_data.iloc[-i] # isolates ith row from the bottom
+        test_taken = "Date: "+ str(this_row['field4'])+" Time: "+str(this_row['field5'])
+        # create a new packet associated for user 
+        p = Packet(body=str(this_row['field3']), author=user, test_type=str(this_row['field2']), test_taken=test_taken) # field2 = test_type
+        packet_queue.put(p) # add new packet to our packet_queue
+    return packet_queue
+
+
+def new_packet(user, packet_queue):
+    try:
+        while True:
+            pack = packet_queue.get(block=False)
+            done = False
+            while not done:
+                try:
+                    # do stuff
+                    # add new packet to db
+                    db.session.add(pack)
+                    db.session.commit()
+                    done = True
+                except Exception as e:
+                    pass # just try again to do stuff
+            packet_queue.task_done()
+    except Empty: # no more packets in the queue
+        pass
+           
+
 # this function deletes all packets for the given user
 def delete_all_packets(user):
     packets = user.packets.all() 
@@ -260,7 +307,6 @@ def open_packet(username, packet_id):
 @app.route('/open_packet/<username>/test_chart')
 def test_chart(username):
     user = User.query.filter_by(username=username).first_or_404()
-    #pack = Packet.query.filter_by(id=packet_id).first_or_404()
     return render_template('test_chart.html', user=user)
 
 

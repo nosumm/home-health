de4ab290001a3e8fc1e286a8a0a10ebf431da76d
SongXP123 <sxp0728@126.com>
2022-03-02 17:50:10 -0800

    more changes on about

--- website/app.py
+++ website/app.py
@@ -26,6 +26,48 @@ import pandas as pd
 from pandas import DataFrame
 from io import StringIO
 
+"""
+from ast import Pass
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+import app
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+    
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    DOB = StringField('DOB (MM-DD-YYYY)', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign Up')
+    admin_code = StringField('Admin Code (optional)') #, validators=[EqualTo(people.admin_code)])
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+        
+        
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    DOB = StringField('DOB (MM-DD-YYYY)', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+"""
 app = Flask(__name__)
 app.config.from_object(Config)
 login = LoginManager(app)

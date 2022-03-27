2be3ec1a40e18567385743d2043ec468861f3cb6
nosum <noahstaveley@gmail.com>
2022-03-27 14:55:31 -0700

    test again

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
     #    'postgres://', 'postgresql://') or \
     #    'sqlite:///' + os.path.join(basedir, 'app.db')
-     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-         'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
